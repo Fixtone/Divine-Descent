c9e97a9e2fe327260d69fe626b8f63a2f8b08221
Tonet Cadpevila Piro <tonet.capdevila@socialpoint.es>
2024-11-29 18:23:57 +0100

    UI ROoot

---
+++ Assets/Code/Scripts/Ui/UIRoot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// UI Root class, used for storing references to UI views.
+/// </summary>
+public class UIRoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameView gameView;
+    public GameView GameView => gameView;
+}
---
+++ Assets/Code/Scripts/Ui/Views/GameView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Game view class.
+/// Passes button events.
+/// </summary>
+public class GameView : BaseView
+{
+    // Events to attach to.
+    public UnityAction OnPauseClicked;
+    public UnityAction OnFinishClicked;
+
+    /// <summary>
+    /// Method called by Pause Button.
+    /// </summary>
+    public void PauseClick()
+    {
+        OnPauseClicked?.Invoke();
+    }
+
+    /// <summary>
+    /// Method called by Finish Button.
+    /// </summary>
+    public void FinishClick()
+    {
+        OnFinishClicked?.Invoke();
+    }
+}
